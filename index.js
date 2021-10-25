e2f92913b4fbb538cc0f670ae1402d715ca74ffb
Luis Bedoya <luis.bedoya@bhdleon.com.do>
2021-10-24 20:23:54 -0500

    terminacion de curso platzi
    terminacion de curso platzi

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const cors = require('cors');
 const routerApi = require('./routes');
+const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/errorHandler');
 const app = express();
 const port = 3000;
 
 // middleware que sirve para admitir data de tipo JSON
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) =>{
   res.send("Hola mi server en Express");
@@ -15,6 +18,10 @@ app.get("/nueva-ruta", (req, res) =>{
 });
 
 routerApi(app);
+app.use(logErrors);
+app.use(boomErrorHandler);
+app.use(errorHandler);
+
 
 app.listen(port, () =>{
   console.log("My port: " + port);
--- routes/products.router.js
+++ routes/products.router.js
@@ -1,19 +1,14 @@
 const express = require("express");
-const faker = require('faker');
+const ProductServices = require('../Services/productService');
+const validatorHandler = require('../middlewares/validator.handler');
+const { createProductSchema, updateProductSchema, getProductSchema } = require('../schemas/product.schema');
+
 
 const router = express.Router();
+const service = new ProductServices();
 
-router.get("/", (req, res) =>{
-  const products = [];
-  const { size } = req.query;
-  const limit = size || 10;
-  for (let index = 0; index < limit; index++) {
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      image: faker.image.imageUrl()
-    });
-  }
+router.get("/", async (req, res) =>{
+  const products = await service.find();
   res.json(products);
 });
 
@@ -21,46 +16,70 @@ router.get('/filter', (req, res) => {
   res.send('soy un filter');
 });
 
-router.get('/:id', (req, res) => {
-  const { id } = req.params;
-  if (id === '999') {
-    res.status(404).json({
-      message: "id no encontrado"
+router.get('/:id',
+  validatorHandler(getProductSchema, 'params'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+    const product = await service.findOne(id);
+    if (product) {
+      res.status(200).json(product);
+    } else {
+      res.status(404).json({
+        message:'id no encontrado'
+      })
+    }
+    } catch (error) {
+      next(error)
+    }
+});
+
+router.post('/',
+  validatorHandler(createProductSchema, 'body'),
+  async (req, res) => {
+  const body = req.body;
+  const newProduct = await service.create(body);
+  if (newProduct) {
+    res.status(201).json({
+      message: 'Creacion',
+      data: newProduct
     });
   } else {
-    res.status(200).json(
-      {
-        id,
-        name: "mac",
-        price: 1000
-      }
-    );
+    res.status(500).json({
+      message:'Ocurrio un error'
+    })
   }
+
 });
 
-router.post('/', (req, res) => {
-  const body = req.body;
-  res.status(201).json({
-    message: 'Creacion',
-    data: body
-  });
+router.patch('/:id',
+  validatorHandler(getProductSchema, 'params'),
+  validatorHandler(updateProductSchema, 'body'),
+  async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const body = req.body;
+    const product = await service.update(id, body);
+    res.json({
+      message: 'Actualizacion',
+      data: product,
+      id: id
+    });
+  } catch (error) {
+    next(error);
+    // res.status(404).json({
+    //   message: error.message
+    // });
+  }
+
 });
 
-router.patch('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   const { id } = req.params;
-  const body = req.body;
-  res.json({
-    message: 'Actualizacion',
-    data: body,
-    id: id
-  });
-});
-
-router.delete('/:id', (req, res) => {
-  const { id } = req.params;
+  const rta = await service.delete(id);
   res.json({
     message: 'Eliminar',
-    id: id
+    id: rta
   });
 });
 
